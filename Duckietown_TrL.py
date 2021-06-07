48c72a465347e506f00c7d5abf138f3d8278b123
henri-xh <84185228+henri-xh@users.noreply.github.com>
2021-06-07 11:55:40 +0300

    Update Duckietown_TrL.py

--- Duckietown_TrL.py
+++ Duckietown_TrL.py
@@ -8,31 +8,27 @@ G.setmode(G.BCM)
 
 
 #1st set
-G.setup(22, G.OUT) #No.15
-G.setup(23, G.OUT) #No.16
-G.setup(27, G.OUT) #No.13
-#GND No.14
+G.setup(27, G.OUT) #r1
+G.setup(22, G.OUT) #y1
+G.setup(23, G.OUT) #g1
 
 
 #2nd set
-G.setup(10, G.OUT) #No.19
-G.setup(9, G.OUT) #No.21
-G.setup(25, G.OUT) #No.22
-#GND No.20
+G.setup(4, G.OUT) #r2
+G.setup(14, G.OUT) #y2
+G.setup(15, G.OUT) #g2
 
 
 #3rd set
-G.setup(26, G.OUT) #No.37
-G.setup(20, G.OUT) #No.38
-G.setup(21, G.OUT) #No.40
-#GND No.39
+G.setup(26, G.OUT) #r3
+G.setup(20, G.OUT) #y3
+G.setup(21, G.OUT) #g3
 
 
 #4th set
-G.setup(5, G.OUT) #No.27
-G.setup(6, G.OUT) #No.29
-G.setup(12, G.OUT) #No.28
-#GND No.30
+G.setup(5, G.OUT) #r4
+G.setup(6, G.OUT) #y4
+G.setup(12, G.OUT) #g4
 
 
 running = True
@@ -41,71 +37,35 @@ count = 0
 while running:
     
     
-    #1st set
-    G.output(22, G.HIGH)
+    G.output(27, G.HIGH) #r1
+    G.output(26, G.HIGH) #r3
+    G.output(15, G.HIGH) #g2
+    G.output(12, G.HIGH) #g4
     sleep(10)
-    G.output(22, G.LOW)
-    print ('Red_1 done.')
+    G.output(27, G.LOW) #r1
+    G.output(26, G.LOW) #r3
+    G.output(15, G.LOW) #g2
+    G.output(12, G.LOW) #g4
+   
     
-    G.output(23, G.HIGH)
-    sleep(10)
-    G.output(23, G.LOW)
-    print ('Green_1 done.')
+    G.output(22, G.HIGH) #y1
+    G.output(20, G.HIGH) #y3
+    G.output(14, G.HIGH) #y2
+    G.output(6, G.HIGH) #y4
+    sleep(1.5)
+    G.output(22, G.LOW) #y1
+    G.output(20, G.LOW) #y3
+    G.output(14, G.LOW) #y2
+    G.output(6, G.LOW) #y4
 
-    G.output(27, G.HIGH)
-    sleep(1.5)
-    G.output(27, G.LOW)
-    print ('Yellow_1 done.')
-    
-    
-    #3rd set
-    G.output(26, G.HIGH)
-    sleep(10)
-    G.output(26, G.LOW)
-    print ('Red_3 done.')
-    
-    G.output(20, G.HIGH)
-    sleep(10)
-    G.output(20, G.LOW)
-    print ('Green_3 done.')
 
-    G.output(21, G.HIGH)
-    sleep(1.5)
-    G.output(21, G.LOW)
-    print ('Yellow_3 done.')
-    
-    
-    
-    
-    #2nd set
-    G.output(9, G.HIGH)
+    G.output(23, G.HIGH) #g1
+    G.output(21, G.HIGH) #g3
+    G.output(4, G.HIGH) #r2
+    G.output(5, G.HIGH) #r4
     sleep(10)
-    G.output(9, G.LOW)
-    print ('Green_2 done.')
-
-    G.output(25, G.HIGH)
-    sleep(1.5)
-    G.output(25, G.LOW)
-    print ('Yellow_2 done.')
-    
-    G.output(10, G.HIGH)
-    sleep(10)
-    G.output(10, G.LOW)
-    print ('Red_2 done.')
-    
-    
-    #4th set
-    G.output(5, G.HIGH)
-    sleep(10)
-    G.output(5, G.LOW)
-    print ('Green_4 done.')
-    
-    G.output(1, G.HIGH)
-    sleep(1.5)
-    G.output(1, G.LOW)
-    print ('Yellow_4 done.')
-    
-    G.output(0, G.HIGH)
-    sleep(10)
-    G.output(0, G.LOW)
-    print ('Red_4 done.')
+    G.output(23, G.LOW) #g1
+    G.output(21, G.LOW) #g3
+    G.output(4, G.LOW) #r2
+    G.output(5, G.LOW) #r4
+   
